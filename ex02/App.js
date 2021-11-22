7e5a8f91dfe99193ec85cb02ba793696d8700dc8
Richard CHEN <richard.chen32^gmail.com>
2021-11-22 10:56:27 +0100

    commit de ex02

---
+++ ex02/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Button title="Bouton">
+       
+      </Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#9F9F9F',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
